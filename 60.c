8356adc8a90571fe3f95399a1ea8efaabeddedd7
ilakkiyaisaiya <31511905+ilakkiyaisaiya@users.noreply.github.com>
2018-02-21 16:54:20 +0530

    Create 60.c

---
+++ 60.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int f=0,s=1,i,n,next;
+printf("enter the number:  ");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+next=f+s;
+f=s;
+s=next;
+}
+printf("the fibonacci result is %d",next);
+return0;
+}
