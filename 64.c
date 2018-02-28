9799ef151c054e0e74fa91a837604ce0833745d6
ilakkiyaisaiya <31511905+ilakkiyaisaiya@users.noreply.github.com>
2018-02-28 15:51:52 +0530

    Create 64.c

---
+++ 64.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+	int n,m,a;
+	printf("enter the number");
+	scanf("%d%d",&n,&m);
+	a=n+m;
+	if(a%2==0)
+	{
+		printf("even");
+	}
+	else
+	{
+printf("odd");
+}
+}
