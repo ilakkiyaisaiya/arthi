5c8c272e014018458f741ef5c36efd76a66cc512
ilakkiyaisaiya <31511905+ilakkiyaisaiya@users.noreply.github.com>
2018-03-02 15:47:51 +0530

    Create 68.c

---
+++ 68.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int a;
+	scanf("%d",&a);
+	if(a%7==0)
+	{
+		printf("the given no is multiple of 7");
+	}
+	else
+	{
+		printf("the given no is not a multiple of 7");
+	}
+	}
+
