f5fcd85c94425c62b3a98f37b9feff03239caedb
ilakkiyaisaiya <31511905+ilakkiyaisaiya@users.noreply.github.com>
2018-03-12 16:22:16 +0530

    Create 63.c

---
+++ 63.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char a[20]="ilakkiya isaiya";
+    int i;
+    int count=0;
+    for(i=0;i<=20;i++)
+    {
+        if(a[i]==' ')
+    
+        count++;
+    }
+    printf("%d",count+1);
+}
