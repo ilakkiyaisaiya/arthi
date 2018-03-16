63d8f1013e588656e1a0a93adccdd5c37d347d68
ilakkiyaisaiya <31511905+ilakkiyaisaiya@users.noreply.github.com>
2018-03-16 16:45:21 +0530

    Create 79.c

---
+++ 79.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a1,a2,n;
+    printf("ënter the number");
+    scanf("%d%d",&a1,&a2);
+    n=a1*a2;
+    if(a1==a2)
+    {
+        printf("ÿes");
+    }
+    else
+    {
+        printf("no");
+    }
+}
